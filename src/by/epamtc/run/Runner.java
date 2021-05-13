b3717b74bdbf440aa7e0b4d345fe24802aec45a0
VoloshchukD <voloshchukd7@gmail.com>
2021-05-13 22:09:22 +0300

    Exceptions in main refactoring

--- src/by/epamtc/run/Runner.java
+++ src/by/epamtc/run/Runner.java
@@ -11,31 +11,58 @@ import by.epamtc.service.TextReader;
 import java.io.FileNotFoundException;
 
 public class Runner {
-    public static void main(String[] args) throws FileNotFoundException,
-            WrongFileNameException, TextHandlerIndexOutOfBoundsException, NoSuchTextException {
-        String text = TextReader.readTextFromFile("data.txt");
-        System.out.println(StringTextHandler.replaceEachWordLetter(text, "P", 3));
-        System.out.println(CharTextHandler.replaceEachWordLetter(text, 'P', 3));
-        System.out.println(RegularExpressionTextHandler.replaceEachWordLetter(text, 'P', 3));
+    public static void main(String[] args) {
+        String text = null;
+        try {
+            text = TextReader.readTextFromFile("data.txt");
+            System.out.println(StringTextHandler.replaceEachWordLetter(text, "P", 3));
+            System.out.println(CharTextHandler.replaceEachWordLetter(text, 'P', 3));
+            System.out.println(RegularExpressionTextHandler.replaceEachWordLetter(text, 'P', 3));
+        } catch (FileNotFoundException | WrongFileNameException
+                | NoSuchTextException | TextHandlerIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
 
         text = "    PAhfPA,    apsfhjP,    sflkjPO   ";
-        System.out.println(StringTextHandler.correctWrongLetter(text));
-        System.out.println(CharTextHandler.correctWrongLetter(text));
-        System.out.println(RegularExpressionTextHandler.correctWrongLetter(text));
+        try {
+            System.out.println(StringTextHandler.correctWrongLetter(text));
+            System.out.println(CharTextHandler.correctWrongLetter(text));
+            System.out.println(RegularExpressionTextHandler.correctWrongLetter(text));
+        } catch (NoSuchTextException e) {
+            e.printStackTrace();
+        }
 
         text = "    hfP,   sfhd,   sfg,   dsdaf.   ";
-        System.out.println(StringTextHandler.replaceWordsWithSubstring(text, "AAAAA", 3));
-        System.out.println(CharTextHandler.replaceWordsWithSubstring(text, "AAAAA", 3));
-        System.out.println(RegularExpressionTextHandler.replaceWordsWithSubstring(text, "AAAAA", 3));
+        try {
+            System.out.println(StringTextHandler.replaceWordsWithSubstring(text, "AAAAA", 3));
+            System.out.println(CharTextHandler.replaceWordsWithSubstring(text, "AAAAA", 3));
+            System.out.println(RegularExpressionTextHandler.replaceWordsWithSubstring(
+                    text, "AAAAA", 3));
+        } catch (NoSuchTextException e) {
+            e.printStackTrace();
+        } catch (TextHandlerIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
 
         text = "   hello, its,    me.    ";
-        System.out.println(StringTextHandler.removeAllCharacters(text));
-        System.out.println(CharTextHandler.removeAllCharacters(text));
-        System.out.println(RegularExpressionTextHandler.removeAllCharacters(text));
+        try {
+            System.out.println(StringTextHandler.removeAllCharacters(text));
+            System.out.println(CharTextHandler.removeAllCharacters(text));
+            System.out.println(RegularExpressionTextHandler.removeAllCharacters(text));
+        } catch (NoSuchTextException e) {
+            e.printStackTrace();
+        }
 
         text = "   Fojd,    Hkd,    Pro,    jkds,   jkh.  ";
-        System.out.println(StringTextHandler.removeWordsStartingWithConsonant(text, 3));
-        System.out.println(CharTextHandler.removeWordsStartingWithConsonant(text, 3));
-        System.out.println(RegularExpressionTextHandler.removeWordsStartingWithConsonant(text, 3));
+        try {
+            System.out.println(StringTextHandler.removeWordsStartingWithConsonant(text, 3));
+            System.out.println(CharTextHandler.removeWordsStartingWithConsonant(text, 3));
+            System.out.println(RegularExpressionTextHandler.removeWordsStartingWithConsonant(text, 3));
+        } catch (NoSuchTextException e) {
+            e.printStackTrace();
+        } catch (TextHandlerIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
     }
 }
